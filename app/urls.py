bc551172bfc8df45cdd78c45ff8ac5a64f37dba0
alejandro.cordoba <alejo.artia@gmail.com>
2022-09-25 13:14:08 +0200

    add: sync_to_async and start module csv_processing.py

--- app/urls.py
+++ app/urls.py
@@ -6,3 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.upload, name="upload")
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
+from asgiref.sync import sync_to_async
 from django.shortcuts import render
+import asyncio
+from time import sleep
+import httpx
+from django.http import HttpResponse
 
 from .models import UploadSync
 
+"""using the decorator sync_to_async This means that the synchronous function, http_call_sync, will be run in a new thread. Review the docs for more 
+info. as we need interact with the db is not posible to use only library asyncIO without to use sync_to_async"""
 
+
+@sync_to_async
 def upload(request):
     if request.method == 'POST':
         images = request.FILES.getlist('images')
